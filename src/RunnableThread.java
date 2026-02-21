f235622bf3c6cd653b8dd98636f38cf9835b093b
Babithosh <ellu250071@cet.ac.in>
2026-02-21 17:10:44 +0530

    added runnable interface multithreading code

--- src/RunnableThread.java
+++ src/RunnableThread.java
@@ -1,6 +1,12 @@
 class MyRunnable implements Runnable {
     // TODO: Override run() method
     // Loop from 1 to 5 and print numbers separated by space
+
+	public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + " ");
+        }
+    }
 }
 
 public class RunnableThread {
@@ -8,5 +14,9 @@ public class RunnableThread {
         // TODO: Create an instance of MyRunnable
         // TODO: Create a Thread object passing the runnable instance
         // TODO: Start the thread
+
+	MyRunnable obj = new MyRunnable();
+        Thread t = new Thread(obj);
+        t.start();
     }
 }
